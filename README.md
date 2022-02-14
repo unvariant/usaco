ee08ef0cd4eadf4e9a6927a17e520ea3b0683b86
unvariant <primalpeashooter200@gmail.com>
2022-02-14 00:39:07 +0000

    solved milking order

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-| problem name      | problem link                                              | date started | date finished | status               |
-|:------------------|:----------------------------------------------------------|:-------------|:--------------|:---------------------|
-| blocked billboard | http://usaco.org/index.php?page=viewproblem2&cpid=759     | 2/11/22      | 2/11/22       | (solved)             |
-| grass planting    | http://www.usaco.org/index.php?page=viewproblem2&cpid=894 | 2/11/22      | 2/11/22       | (looked at solution) |
-| milking order     | http://www.usaco.org/index.php?page=viewproblem2&cpid=832 | 2/11/22      | 2/11/22       | (in progress)        |
+| problem name      | problem link                                               | date started | date finished | status               | difficulty |
+|:------------------|:---------------------------------------------------------- |:-------------|:--------------|:---------------------|:-----------|
+| blocked billboard | http://usaco.org/index.php?page=viewproblem2&cpid=759      | 2/11/22      | 2/11/22       | (solved)             | bronze     |
+| grass planting    | http://www.usaco.org/index.php?page=viewproblem2&cpid=894  | 2/11/22      | 2/11/22       | (looked at solution) | silver     |
+| milking order     | http://www.usaco.org/index.php?page=viewproblem2&cpid=832  | 2/11/22      | 2/13/22       | (solved)             | bronze     |
+| swapity swap      | http://www.usaco.org/index.php?page=viewproblem2&cpid=1013 | 2/13/22      |               | (in progress)        | bronze     |
--- milking_order/main.cpp
+++ milking_order/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 using namespace std;
 
 int N, M, K;
-vector<int>
+vector<int> h;
+int cows[101] = {0};   // cows[i] contains the position of cow i
+int order[101] = {0};
 
 int main() {
     ifstream fin ("milkorder.in");
@@ -11,4 +14,60 @@ int main() {
 
     fin >> N >> M >> K;
 
+    for(int i = 0; i < M; i++) {
+        int a; fin >> a;
+        h.push_back(a);
+    }
+
+    for(int i = 0; i < K; i++) {
+        int a, b; fin >> a >> b;
+        if(a == 1) {
+            fout << b << "\n";
+            return 0;
+        }
+        cows[a] = b;
+        // indicates that cow i is at position cows[i]
+
+        order[b] = a;
+        // indicates that at cow a is at position b - 1
+    }
+
+    int right = N, i = M - 1;
+    for(; i > -1; i--) {
+        if(h[i] == 1) {
+            break;
+        } else {
+            if(cows[h[i]] != 0) {
+                right = cows[h[i]];
+            } else {
+                while(order[right] != 0) {
+                    right--;
+                }
+                order[right] = h[i];
+                right--;
+            }
+        }
+    }
+
+    int result = 1;
+    if(i > -1) {
+        for(int j = 0; j < i + 1; j++) {
+            if(cows[h[j]] != 0) {
+                result = cows[h[j]];
+            } else {
+                while(order[result] != 0) {
+                    result++;
+                }
+                order[result] = h[j];
+                result++;
+            }
+        }
+        result--;
+    } else {
+        while(order[result] != 0) {
+            result++;
+        }
+    }
+
+    fout << result << "\n";
 }
